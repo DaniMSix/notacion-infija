a842117801b1018b99d422954627897e872b87b5
Hellcat711 <116575784+Hellcat711@users.noreply.github.com>
2022-10-24 11:23:39 -0500

    Update Pila.cpp
    Signed-off-by: Hellcat711 <116575784+Hellcat711@users.noreply.github.com>

--- Pila.cpp
+++ Pila.cpp
@@ -1,4 +1,53 @@
+//Miniproyecto 5. Pilas
+
 #include <iostream>
 using namespace std;
-int main(){
+
+class Nodo{
+	public:
+		int dato;
+		Nodo *siguientePtr;
+};
+
+class Pila{
+	private:
+		Nodo *topePtr;
+		bool estaVacia();
+	public:
+		Pila();
+		~Pila();
+		void mete(int valor);
+		int saca();
+		int nodoInicio();
+		void mueve(Pila &p1);
+		void muestra();
+		int cantidadElementos();
+		int nodoMayor();
+		void eliminarNodo(int referencia);
+		void concatena(Pila &p1);
+};
+
+Pila::Pila(){
+	topePtr = NULL;
 }
+
+Pila::~Pila(){
+	while(!estaVacia()){
+		saca();
+	}
+}
+
+bool Pila::estaVacia(){
+	return topePtr == NULL;
+}
+
+int main(){
+  
+  return 0;
+}
+
+
+
+
+
+
